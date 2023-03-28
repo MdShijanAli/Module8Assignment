aa8d68c3f77f91f81a09cd96e9e90a0133768530
FreelancerShijan <freelancershijan@gmail.com>
2023-03-28 21:49:33 +0600

    update

--- Registration.php
+++ Registration.php
@@ -1,115 +1,59 @@
 <?php
-if (isset($_POST['register'])) {
-    $first_name = test_input($_POST["first_name"]);
-    $last_name = test_input($_POST["last_name"]);
-    $email = test_input($_POST["email"]);
-    $password = test_input($_POST["password"]);
-    $confirm_password = test_input($_POST["confirm_password"]);
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Get form data
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $confirm_password = $_POST['confirm_password'];
 
-    if (empty($first_name) || empty($last_name) || empty($email) || empty($password) || empty($confirm_password)) {
-        $allField = "All fields are  and must not be empty";
-    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $emaill = "Invalid email format";
-    } elseif ($password != $confirm_password) {
-        $pass = "Password and Confirm Password fields must match";
-    } else {
-        header('location: successfull.html');
-    }
-} else {
-    $notError = "";
-}
+    // Save user data to database (you will need to set up your own database)
 
-function test_input($data)
-{
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
+    // Redirect to welcome page
+    header('Location: welcome.php?first_name=' . urlencode($first_name));
+    exit;
 }
 ?>
 
-
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registration Form</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.16/dist/tailwind.min.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+
 </head>
 
-<body>
-    <div class="h-screen flex flex-col items-center justify-center bg-gray-200">
-        <h1 class="text-4xl font-bold mb-8">Registration Form</h1>
-        <form class="w-full max-w-sm p-6 bg-white rounded-lg shadow-lg" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+<body class="bg-gray-400">
+    <h1 class="text-center text-3xl font-semibold mt-20">Registration Form</h1>
+    <div class="bg-white w-1/3 mx-auto my-10 ">
 
+        <form class="p-5" method="post">
             <div class="mb-4">
                 <label for="first_name" class="block text-gray-700 font-bold mb-2">First Name:</label>
-                <input type="text" id="first_name" name="first_name" class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Enter First Name">
+                <input type="text" id="first_name" name="first_name" class="shadow  border rounded w-full py-2 px-3 bg-gray-700 text-white" required>
             </div>
             <div class="mb-4">
                 <label for="last_name" class="block text-gray-700 font-bold mb-2">Last Name:</label>
-                <input type="text" id="last_name" name="last_name" class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Enter Last Name">
-            </div>
-
-            <div class="mb-4">
-                <label for="email" class="block text-gray-700 font-bold mb-2">Email Address:</label>
-                <input type="email" id="email" name="email" class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Enter Email Address">
-            </div>
-
-            <div class="mb-4">
-                <label for="password" class="block text-gray-700 font-bold mb-2">Enter Password</label>
-                <input type="password" id="password" name="password" class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Enter Password">
+                <input type="text" id="last_name" name="last_name" class="shadow  border rounded w-full py-2 px-3 bg-gray-700 text-white" required>
             </div>
             <div class="mb-4">
-                <label for="confirm_password" class="block text-gray-700 font-bold mb-2">Confirm Password</label>
-                <input type="password" id="confirm_password" name="confirm_password" class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Confirm Password">
-            </div>
-
-
-            <div class="mb-4">
-                <?php
-                if (isset($allField)) {
-                    echo "<p class='text-red-800'>$allField</p>";
-                }
-                ?>
+                <label for="email" class="block text-gray-700 font-bold mb-2">Email:</label>
+                <input type="email" id="email" name="email" class="shadow  border rounded w-full py-2 px-3 bg-gray-700 text-white" required>
             </div>
             <div class="mb-4">
-                <?php
-                if (isset($emaill)) {
-                    echo "<p class='text-red-800'>$emaill</p>";
-                }
-                ?>
+                <label for="password" class="block text-gray-700 font-bold mb-2">Password:</label>
+                <input type="password" id="password" name="password" class="shadow  border rounded w-full py-2 px-3 bg-gray-700 text-white" required>
             </div>
             <div class="mb-4">
-                <?php
-                if (isset($pass)) {
-                    echo "<p class='text-red-800'>$pass</p>";
-                }
-                ?>
+                <label for="confirm_password" class="block text-gray-700 font-bold mb-2">Confirm Password:</label>
+                <input type="password" id="confirm_password" name="confirm_password" class="shadow  border rounded w-full py-2 px-3 bg-gray-700 text-white" required>
             </div>
-            <div class="mb-4">
-                <?php
-                if (isset($notError)) {
-                    echo $notError;
-                }
-                ?>
-            </div>
-
-
-            <div class="flex items-center justify-evenly">
-                <button name="register" type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-                    Register
-                </button>
-                <a href="/Login.php">
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-                        Login
-                    </button>
-                </a>
+            <div class="flex items-center justify-between">
+                <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Register</button>
             </div>
         </form>
+    </div>
 
 </body>
 
---
+++ Welcome.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Welcome</title>
+</head>
+
+<body>
+    <?php
+    $first_name = $_GET['first_name'];
+    echo "<h1>Welcome, $first_name!</h1>";
+    ?>
+</body>
+
+</html>
